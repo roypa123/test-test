4f58e0e12c7834f9b547b68fab90f077f488856a
Roy-P-A <roy.pa@nintriva.io>
2026-06-09 12:05:53 +0530

    fix: some changes

--- users-app/vite.config.ts
+++ users-app/vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "users",
+      filename: "remoteEntry.js",
+
+      exposes: {
+        "./UsersPage": "./src/UsersPage.tsx",
+      },
+
+      shared: ["react", "react-dom"],
+    }),
+
+  ],
+
+  build: {
+    target: "esnext",
+  },
+
+  server: {
+    port: 5002,
+  },
+
+
 })
